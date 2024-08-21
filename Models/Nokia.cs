4ab0a346903cec0f2d7a7eb40b6633427ffd5b79
asl <anderson.s.louzada@gmail.com>
2024-08-21 14:22:36 -0300

    feat: implementa as funcionalidades solicitadas seguindo o paradigma POO

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,13 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia(string numero, string modelo, string IMEI, int memoria) : Smartphone(numero, modelo, IMEI, memoria)
     {
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no Nokia...");
+        }
     }
 }
